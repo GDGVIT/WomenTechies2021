0d56911954a3c55580cb35a0e90b4ad2d5b014cf
Krish Chatterjie <chatterjie.krish@gmail.com>
2021-02-09 14:07:18 +0530

    fix: changed timing of hack

--- src/components/Hackathon.js
+++ src/components/Hackathon.js
@@ -9,7 +9,7 @@ const Hackathon = React.forwardRef(({ setHack }, ref) => {
         <div className='hack-content'>
           <img className='hack-bg' src={hackBg} alt='bg' />
           <div className='home-title'>About Hackathon</div>
-          <p>This 24-hour hackathon aims to provide talented and independent women with the platform and resources to help them grow in their desired field and contribute to society with impactful and beneficial solutions.</p>
+          <p>This 36-hour hackathon aims to provide talented and independent women with the platform and resources to help them grow in their desired field and contribute to society with impactful and beneficial solutions.</p>
           <p><b>There is a minimum requirement of 50% female participation in each team,</b> to ensure and promote women’s participation in tech and tech-related events. </p>
           <p>WomenTechies will be an opportunity for the budding developers to showcase their potential and skills under the mentorship of experts and come up with imperceptible ideas and ground-breaking solutions to problems that could potentially solve the woes of thousands of people.</p>
         </div>
