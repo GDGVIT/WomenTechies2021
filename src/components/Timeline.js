1177fdf2900a4cff30f8c69fc34b6da5ef391a26
Krish Chatterjie <chatterjie.krish@gmail.com>
2021-02-27 13:25:14 +0530

    feat: adde timeline section

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,29 +1,27 @@
-import React from 'react'
-// import day1 from './../assets/timeline1.png'
-// import day2 from './../assets/timeline2.svg'
+import React, { useState } from 'react'
+import day1 from './../assets/timeline1.svg'
+import day2 from './../assets/timeline2.svg'
 
 const Timeline = React.forwardRef(({ setTimeline }, ref) => {
+  const [checked, setChecked] = useState(true)
   return (
     <div className='timeline' id='timeline' ref={ref}>
       <div className='timeline-title'>Timeline</div>
       <ul>
         <li>
-          <input type='radio' value='1' name='radio' id='radio1' checked />
-          <label for='radio1'>Day 1</label>
+          <input type='radio' key='1' value='1' name='radio' id='radio1' checked={checked} onChange={(e) => { setChecked(e.target.value) }} />
+          <label htmlFor='radio1'>Day 1</label>
         </li>
         <li>
-          <input type='radio' value='2' name='radio' id='radio2' />
-          <label for='radio2'>Day 2</label>
+          <input type='radio' key='2' value='2' name='radio' id='radio2' checked={!checked} onChange={(e) => { setChecked(!e.target.value) }} />
+          <label htmlFor='radio2'>Day 2</label>
         </li>
       </ul>
-      {/* <div className='timeline-img'>
+      <div className='timeline-img'>
         <div className='timeline-inner'>
-          <img src={day1} alt='Timeline - Day 1' />
+          <img src={checked ? day1 : day2} alt='Timeline' id='timeline-day-img' />
         </div>
       </div>
-      <div className='timeline-img' style={{ display: 'none' }}>
-        <img src={day2} alt='Timeline - Day 2' />
-      </div> */}
     </div>
   )
 })
