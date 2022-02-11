b3db0b93f83cd188ec4948beb4bd9927a3b88790
Pranjal Timsina <pranjal.timsina@live.com>
2022-02-11 14:15:00 +0545

    chore: update dates

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -10,11 +10,11 @@ const Timeline = React.forwardRef(({ setTimeline }, ref) => {
       <ul>
         <li>
           <input type='radio' key='1' value='1' name='radio' id='radio1' checked={checked} onChange={(e) => { setChecked(e.target.value) }} />
-          <label htmlFor='radio1'>14th March</label>
+          <label htmlFor='radio1'>9th April</label>
         </li>
         <li>
           <input type='radio' key='2' value='2' name='radio' id='radio2' checked={!checked} onChange={(e) => { setChecked(!e.target.value) }} />
-          <label htmlFor='radio2'>15th March</label>
+          <label htmlFor='radio2'>10th April</label>
         </li>
       </ul>
       <div className='timeline-img'>
